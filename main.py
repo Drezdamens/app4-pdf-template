8e95ae35d0fe0abd96bd1edf30226b0959978b5c
Lil127moone <lil127moone@gmail.com>
2024-11-06 23:08:39 +0200

    Day 24: Exersice Add lines

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], border=0, ln=1, align="L")
-    pdf.line(10, 21, 200, 21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -25,5 +27,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(150, 150, 150)
         pdf.cell(w=0, h=10, txt=row["Topic"], ln=1, align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("Output")
